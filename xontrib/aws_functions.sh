c061829df9e101a9564d58c7e49d2b558f918c9c
PeterYurkovich <pyurkovi@redhat.com>
2024-04-05 18:53:56 -0400

    Finalize movement to of aws xonsh script to bash

--- xontrib/aws_functions.sh
+++ xontrib/aws_functions.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/zsh
 
 # create a script which checks if we ware logged in with aws, and if not, login
 function check_aws_login() {
-  if aws configure get aws_access_key_id > /dev/null 2>&1
+  if [[ $(aws sts get-caller-identity) != *"ExpiredToken"* ]]
   then
     print -P "\n%F{green}AWS is already logged in%f\n"
     return
@@ -34,54 +34,65 @@ function copy_redhat() {
 }
 
 function delete_buckets() {
+  check_aws_login
   if [[ "$#" -ne 1 ]]; then
-    echo "Bucket name required"
+    print -P "\n%F{red}User name required%f\n"
     return
   fi
-  bucket_names=$(aws s3api list-buckets --output text --no-paginate | grep -oP '(?<=bucketName>)[^<]+')
-  potential_user_buckets=$(echo $bucket_names | grep -oP "^$1")
+  USER_NAME=$1
+  potential_user_buckets=$(aws s3api list-buckets --output json --no-paginate | jq -r ".Buckets[].Name | select(startswith(\"$USER_NAME\"))")
   deletion_count=0
   for potential_user_bucket in $potential_user_buckets; do
-    tag_string=$(aws s3api get-bucket-tagging --bucket $potential_user_bucket --no-paginate)
-    tag_lines=$(echo $tag_string | grep -oP '(?<=TagSet>)[^<]+')
-    for tag_line in $tag_lines; do
-      tag_segment=$(echo $tag_line | grep -oP '(?<=Key>)[^<]+')
-      if [[ $tag_segment == "UserCreator" ]]; then
-        approval=$(echo $tag_line | grep -oP '(?<=Value>)[^<]+')
-        if [[ $approval == $1 ]]; then
-          # aws s3api delete-bucket --bucket $potential_user_bucket --no-paginate
-          echo "Deleted Bucket: $potential_user_bucket"
-          ((deletion_count++))
-        fi
-      fi
-    done
+    tag_string=$(aws s3api get-bucket-tagging --bucket $potential_user_bucket --output json --no-paginate)
+    USER_CREATOR=$(echo $tag_string | jq '.TagSet[] | select(.Key == "UserCreator")' | jq -r '.Value')
+    if [ -z "$USER_CREATOR" ]; then
+      continue
+    fi
+    # confirm delete
+    print -n -P "\n%F{yellow}Delete bucket $potential_user_bucket? (y/n): %f"
+    read APPROVAL
+      if [[ $APPROVAL == "y" ]]; then
+      aws s3api delete-bucket --bucket $potential_user_bucket --no-paginate
+      echo "Deleted Bucket: $potential_user_bucket"
+      ((deletion_count++))
+    fi
   done
   echo "Sucessfully deleted $deletion_count bucket(s)"
 }
 
 function create_bucket() {
+  check_aws_login
   if [[ $# -ne 2 ]]; then
-    echo "Bucket and User name required to create bucket"
+    print -P "\n%F{red}Bucket and User name required to create bucket%f\n"
     return
   fi
   bucket_name=$1
   user_name=$2
   if [[ $bucket_name == "" || $user_name == "" ]]; then
-    echo "Bucket and User name required to create bucket"
+    print -P "\n%F{red}Bucket and User name required to create bucket%f\n"
     return
   fi
   if [[ "$user_name" == "$bucket_name"+^[a-zA-Z0-9] ]]; then
-    echo "Bucket name must start with user name"
+    print -P "\n%F{red}Bucket name must start with user name%f\n"
     return
   fi
   bucket_names=$(aws s3api list-buckets --output text --no-paginate)
   if [[ $bucket_names == *$bucket_name* ]]; then
-    echo "Bucket already exists"
+    print -P "%F{green}Bucket already exists%f\n"
     return
   fi
-  aws s3api create-bucket --no-paginate --bucket $bucket_name --acl private > /dev/null
-  tag_set="TagSet=[{Key=UserCreator,Value=$user_name}]"
-  aws s3api put-bucket-tagging --no-paginate --bucket $bucket_name --tagging $tag_set
-  echo "Bucket created"
+  if aws s3api create-bucket --bucket $bucket_name --acl private > /dev/null
+  then
+    print -P "%F{green}Bucket created%f\n"
+    tag_set="TagSet=[{Key=UserCreator,Value=$user_name}]"
+    if aws s3api put-bucket-tagging --no-paginate --bucket $bucket_name --tagging $tag_set
+    then
+      print -P "%F{green}Bucket tagged%f\n"
+    else
+      print -P "\n%F{red}Bucket tagging failed%f\n"
+    fi
+  else
+    print -P "\n%F{red}Bucket creation failed%f\n"
+  fi
 }
 
