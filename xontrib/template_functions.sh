716ad01670cadd02751764b82166a4f34b69e0cc
PeterYurkovich <pyurkovi@redhat.com>
2024-04-09 10:59:19 -0400

    Figure out bash template files, and start moving over to it

--- xontrib/template_functions.sh
+++ xontrib/template_functions.sh
@@ -1,25 +1,5 @@
 #!/bin/zsh
 
-function oc_apply() {
-  if [ -z "$1" ]; then
-    echo "Must have a filename to apply"
-    return 1
-  fi
-  if ! [ -f "$1" ]; then
-    echo "File $1 does not exist"
-    return 1
-  fi
-  if ! [ -x "$(command -v oc)" ]; then
-    echo "oc is not installed"
-    return 1
-  fi
-  if [ -z "$(oc apply -f @$1)" ]; then
-    echo "Failed to apply"
-    return 1
-  fi
-  return 0
-}
-
 function get_template() {
   if [ -z "$1" ]; then
     echo "Must have a template name to get"
@@ -27,18 +7,51 @@ function get_template() {
   fi
   # Load templates file from templtes folder
   template_location=$HOME/xontrib/yaml/
-  env=$(env | grep REGISTRY_ORG)
-  if [ -z "$env" ]; then
-    echo "No REGISTRY_ORG set"
+  template_location+="$1"
+
+  if ! [ -f "$template_location" ]; then
+    echo "Template at $template_location does not exist"
     return 1
   fi
-  template_name=$env
-  template_name+="-$1"
-  template_name+=".yaml"
-  if ! [ -f "$template_name" ]; then
-    echo "Template $template_name does not exist"
-    return 1
-  fi
-  return 0
+  cat $template_location
 }
 
+function get_tempo() {
+  template=$(get_template "tempo/templates/tempostack-template.tmpl")
+  if [ $? -ne 0 ]; then
+    echo $template
+    return 1
+  fi
+
+  export TEMPOSTACK_NAME="tempo-stack"
+  export NAMESPACE_NAME="openshift-tempo-operator"
+  export SECRET_NAME="secret-one"
+
+  temp_file_location="$HOME/xontrib/yaml/tmp/tempo-$(date +%s).yaml"
+  echo $template | envsubst > $temp_file_location
+  unset TEMPOSTACK_NAME
+  unset NAMESPACE_NAME
+  unset SECRET_NAME
+}
+
+function get_namespace() {
+  if [ -z "$1" ]; then
+    echo "Must have a namespace name to get"
+    return 1
+  fi
+
+  template=$(get_template "templates/namespace-template.tmpl")
+  if [ $? -ne 0 ]; then
+    echo $template
+    return 1
+  fi
+
+  export NAMESPACE_NAME=$1
+
+  temp_file_location="$HOME/xontrib/yaml/tmp/namespace-$(date +%s).yaml"
+  echo $template | envsubst > $temp_file_location
+
+  unset NAMESPACE_NAME
+
+  kaf $temp_file_location
+}
