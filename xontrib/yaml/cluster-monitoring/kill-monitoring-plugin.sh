26c8ba08a92e1ff6e9b107349f7386975a011b88
PeterYurkovich <pyurkovi@redhat.com>
2024-09-16 15:54:30 -0400

    remove handing consoleplugin

--- xontrib/yaml/cluster-monitoring/kill-monitoring-plugin.sh
+++ xontrib/yaml/cluster-monitoring/kill-monitoring-plugin.sh
@@ -3,3 +3,4 @@ oc patch clusterversion version --type json -p "$(cat disable-monitoring.yaml)"
 
 kubectl scale --replicas=0 -n openshift-monitoring deployment/cluster-monitoring-operator
 kubectl scale --replicas=0 -n openshift-monitoring deployment/monitoring-plugin
+kubectl delete ConsolePlugin monitoring-plugin
